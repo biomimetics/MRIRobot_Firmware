ccb9691f9f5468a3f8693c16a41e16d23964514f
naichenzhao <naichenzhao13@gmail.com>
2024-12-10 16:40:54 -0800

    stuff

--- mri_arm/STM_sdk/Core/Src/stm32_startup.c
+++ mri_arm/STM_sdk/Core/Src/stm32_startup.c
@@ -31,20 +31,6 @@ static void MX_USART2_UART_Init(void)
       ;
   }
 }
-// static void MX_USART2_UART_Init(void) {
-//   huart2.Instance = USART2;
-//   huart2.Init.BaudRate = 115200;
-//   huart2.Init.WordLength = UART_WORDLENGTH_8B;
-//   huart2.Init.StopBits = UART_STOPBITS_1;
-//   huart2.Init.Parity = UART_PARITY_NONE;
-//   huart2.Init.Mode = UART_MODE_TX_RX;
-//   huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-//   huart2.Init.OverSampling = UART_OVERSAMPLING_16;
-//   if (HAL_UART_Init(&huart2) != HAL_OK)
-//   {
-//     Error_Handler();
-//   }
-// }
 
 void SystemClock_Config(void)
 {
@@ -87,6 +73,7 @@ int stm32_startup(void)
   MX_USART2_UART_Init();
   SystemClock_Config();
   printf("--------- STM32 START------------ \r\n");
+  HAL_Delay(1000);
 
   main();
 }
