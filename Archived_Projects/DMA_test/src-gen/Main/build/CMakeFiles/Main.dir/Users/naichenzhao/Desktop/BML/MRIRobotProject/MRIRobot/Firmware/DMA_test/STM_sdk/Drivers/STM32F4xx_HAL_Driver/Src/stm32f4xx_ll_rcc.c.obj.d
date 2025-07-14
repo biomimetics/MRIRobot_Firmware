6c010bcac14f580de75ede7853be67cc69e45bac
ronf-ucb <ronf@eecs.berkeley.edu>
2025-07-13 18:05:34 -0700

    Delete Archived_Projects/DMA_test/src-gen/Main/build directory

--- Archived_Projects/DMA_test/src-gen/Main/build/CMakeFiles/Main.dir/Users/naichenzhao/Desktop/BML/MRIRobotProject/MRIRobot/Firmware/DMA_test/STM_sdk/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rcc.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Main.dir/Users/naichenzhao/Desktop/BML/MRIRobotProject/MRIRobot/Firmware/DMA_test/STM_sdk/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rcc.c.obj: \
- /Users/naichenzhao/Desktop/BML/MRIRobotProject/MRIRobot/Firmware/DMA_test/STM_sdk/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_rcc.c
