5b280bc58404d9afde36aeda6a08befa1a6292be
John Atkins <jtatkins440@gmail.com>
2025-08-08 18:13:03 -0700

    Small army of changes.
    Major Changes:
    Added Mode_Machine.lf for switching modes.
    Reduced Main.lf to be very simple and handle defs and rerouting only.
    Added functionality for ModeMachine to enable or disable lf_set() commands in Home_Controller and Motor_Controller
    
    TODO:
    Update ROS_Interface's reading to handle desired state changes from ROS.
    Add more safety features and finish all mode behaviors in Mode_Machine.lf
    Test it to see if any of it actually works like we think it should.

---
+++ mri_arm/sm_config.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#ifndef __SM_CONFIG_H
+#define __SM_CONFIG_H
+
+// Set ModeMachine behavior mode: 0 for IDLE, 1 for HOME, 2 for RUN
+enum mode_enum {
+    SET_IDLE = 0,
+    SET_HOMING = 1,
+    SET_RUN = 2
+  };
+
+#endif /* __SM_CONFIG_H */
+
+
+#ifndef __MOTOR_MODE_ENUM
+#define __MOTOR_MODE_ENUM
+
+  // Set Motor Mode -> velocity or position control
+  typedef enum {
+      MODE_POS = 0U,
+      MODE_VEL = 1U,
+    } MOTOR_MODE;
+  
+
+  // Set Target Mode -> Internal or external control
+  typedef enum {
+      MODE_INT = 0U,
+      MODE_EXT = 1U,
+    } TARGET_SRC;
+
+#endif // __MOTOR_MODE_ENUM
