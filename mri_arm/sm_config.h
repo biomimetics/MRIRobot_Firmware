a996499fb3fc1326b1e70079c5d53c4d83cce0ed
John Atkins <jtatkins440@gmail.com>
2025-08-09 19:23:59 -0700

    Moved the status struct from Mode_Machine to the .h file.
    Still running into lfc compiler error during running its generator, and the error message it gives is not very helpful.

--- mri_arm/sm_config.h
+++ mri_arm/sm_config.h
@@ -1,19 +1,5 @@
 #include <stdio.h>
 
-#ifndef __SM_CONFIG_H
-#define __SM_CONFIG_H
-
-// Set ModeMachine behavior mode: 0 for IDLE, 1 for HOME, 2 for RUN
-typedef enum {
-    SET_IDLE = 0,
-    SET_HOMING = 1,
-    SET_RUN_POS = 2,
-    SET_RUN_VEL = 3
-  } MODE_ENUM;
-
-#endif /* __SM_CONFIG_H */
-
-
 #ifndef __MOTOR_MODE_ENUM
 #define __MOTOR_MODE_ENUM
 
@@ -31,3 +17,27 @@ typedef enum {
     } TARGET_SRC;
 
 #endif // __MOTOR_MODE_ENUM
+
+#ifndef __SM_CONFIG_H
+#define __SM_CONFIG_H
+
+// Set ModeMachine behavior mode: 0 for IDLE, 1 for HOME, 2 for RUN
+typedef enum {
+    SET_IDLE = 0,
+    SET_HOMING = 1,
+    SET_RUN_POS = 2,
+    SET_RUN_VEL = 3
+  } MODE_ENUM;
+
+typedef struct mode_status{
+    MODE_ENUM current_mode_index;
+    bool is_homing_finished;
+    bool enable_homing_command;
+    bool enable_motor_command;
+    bool force_disable_motor_pins;
+    TARGET_SRC motor_controller_internal_external_target_mode;
+    MOTOR_MODE[7] motor_controller_target_sel;
+  } mode_status;
+#endif /* __SM_CONFIG_H */
+
+
